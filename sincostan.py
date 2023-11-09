fc3672a158dd866f20490e84d867eb189abd9fec
yamajunn <amiton1115@gmail.com>
2023-11-09 19:24:47 +0900

    a

--- sincostan.py
+++ sincostan.py
@@ -11,3 +11,16 @@ distance_root = distance_array.sum()
 
 distance = math.sqrt(distance_root)
 print(distance)
+
+display_list = [" " for i in range(8000)]
+for i in range(40):
+    display_list[i*200]="\n"
+
+display_list[80+200*10] = "#"
+display_list[120+200*10] = "#"
+display_list[80+200*30] = "#"
+display_list[120+200*30] = "#"
+
+
+display_str = "".join(display_list)
+print(display_str)
